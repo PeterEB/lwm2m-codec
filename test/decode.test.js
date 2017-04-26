b65136a6e86b25609e8494c5dd499fc485880510
PeterEB <peter.eb9@gmail.com>
2017-04-26 15:36:09 +0800

    bump to v0.0.2

--- test/decode.test.js
+++ test/decode.test.js
@@ -1,9 +1,16 @@
 var expect = require('chai').expect,
-    decode = require('../lib/decode.js');
-    
+    decode = require('../lib/decode.js'),
+    cutils = require('../lib/cutils.js');
+
 describe('decode - Functional Check', function () {
     it('#.decode(link)', function () {
         expect(decode('link', '</1/2>;pmin=10;pmax=60,</1/2/1>,</1/2/2>')).to.be.eql({ path: '/1/2', attrs: { pmin: 10, pmax: 60 }, resrcList: ['/1/2/1', '/1/2/2'] });
+    });
+
+    it('#.decode(tlv)', function () {
+        expect(decode('tlv', '/3303', cutils.bufferFrom([0x08, 0x00, 0x0b, 0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x63, 0x08, 0x01, 0x0b, 0xe4, 0x16, 0x44, 0x42, 0xb2, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x66]))).to.be.eql({ '0': { '5700': 31, '5701': 'c' }, '1': { '5700': 89, '5701': 'f' } });
+		expect(decode('tlv', '/3303/0', cutils.bufferFrom([0x08, 0x00, 0x0b, 0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x63]))).to.be.eql({ '5700': 31, '5701': 'c' });
+		expect(decode('tlv', '/3303/0/5700', cutils.bufferFrom([0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00]))).to.be.eql(31);
     });
 
     it('#.decode(json)', function () {
--- test/encode.test.js
+++ test/encode.test.js
@@ -1,11 +1,18 @@
 var expect = require('chai').expect,
-    encode = require('../lib/encode.js');
+    encode = require('../lib/encode.js'),
+    cutils = require('../lib/cutils.js');
     
 describe('encode - Functional Check', function () {
     it('#.encode(link)', function () {
         expect(encode('link', 'x', { 1: {  0: 'x', 1: 5 }, 2: {  0: true, 1: 0 }}, { pmin: 10, pmax: 60 })).to.be.eql('</x>;pmin=10;pmax=60,</x/1/0>,</x/1/1>,</x/2/0>,</x/2/1>');
-		expect(encode('link', 'x/y', {  0: 'x', 1: 5 }, { pmin: 10, pmax: 60 })).to.be.eql('</x/y>;pmin=10;pmax=60,</x/y/0>,</x/y/1>');
-		expect(encode('link', 'x/y/z', 10, { pmin: 10, pmax: 60 })).to.be.eql('</x/y/z>;pmin=10;pmax=60');
+        expect(encode('link', 'x/y', {  0: 'x', 1: 5 }, { pmin: 10, pmax: 60 })).to.be.eql('</x/y>;pmin=10;pmax=60,</x/y/0>,</x/y/1>');
+        expect(encode('link', 'x/y/z', 10, { pmin: 10, pmax: 60 })).to.be.eql('</x/y/z>;pmin=10;pmax=60');
+    });
+
+    it('#.encode(tlv)', function () {
+        expect(encode('tlv', '/3303', {0: {5700: 31, 5701: 'c'}, 1:{5700: 89, 5701: 'f'}})).to.be.eql(cutils.bufferFrom([0x08, 0x00, 0x0b, 0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x63, 0x08, 0x01, 0x0b, 0xe4, 0x16, 0x44, 0x42, 0xb2, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x66]));
+        expect(encode('tlv', '/3303/0', {5700: 31, 5701: 'c'})).to.be.eql(cutils.bufferFrom([0x08, 0x00, 0x0b, 0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00, 0xe1, 0x16, 0x45, 0x63]));
+        expect(encode('tlv', '/3303/0/5700', 31)).to.be.eql(cutils.bufferFrom([0xe4, 0x16, 0x44, 0x41, 0xf8, 0x00, 0x00]));
     });
 
     it('#.encode(json)', function () {
